99e88d0138904b68766f36c306a8d790657d9778
LiuAir <okernelpro@me.com>
2016-01-12 01:38:54 +0800

    add style to view

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -2,17 +2,34 @@ var React = require('react-native');
 var {
     Text,
     View,
-    AppRegistry
+    AppRegistry,
+    StyleSheet
 } = React;
 
 var StopWatch = React.createClass({
     render: function() {
-        return <View>
-            <Text>
-                00:00:00
-            </Text>
-            {this.startStopFunction()}
-            {this.lapButton()}
+        return <View style={styles.container}>
+
+            <View style={[styles.header,this.border('yellow')]}>
+                <View style={this.border('red')}>
+                    <Text >
+                        00:00:00
+                    </Text>
+                </View>
+
+                <View style={this.border('green')}>
+                    {this.startStopFunction()}
+                    {this.lapButton()}
+                </View>
+
+            </View>
+
+            <View style={[styles.footer,this.border('blue')]}>
+                <Text>
+                    I am a list of Laps
+                </Text>
+            </View>
+
         </View>
     },
     startStopFunction:function(){
@@ -28,6 +45,12 @@ var StopWatch = React.createClass({
                 Lap
             </Text>
         </View>
+    },
+    border:function(color){
+        return {
+            borderColor: color,
+            borderWidth:4
+        }
     }
 
 });
@@ -35,5 +58,18 @@ var StopWatch = React.createClass({
 // AppRegistry.registerComponent('stopwatch',function(){
 //     return StopWatch;
 // });
+
+var styles = StyleSheet.create({
+    container: {
+        flex:1, // Fill the entire the screen
+        alignItems:'stretch'
+    },
+    header: { // Yellow
+        flex:1
+    },
+    footer: { //Blue
+        flex:1
+    }
+});
 
 AppRegistry.registerComponent('stopwatch', () => StopWatch);
